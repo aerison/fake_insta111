60f7cb23d552fc675dfb37addc819bbd73a32842
aerison <sonsoo1224@gmail.com>
2018-06-25 06:37:42 +0000

    fakeinsta background

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def new
+  end
+
+  def create
+
+    @comment=Post.find(params[:post_id]).comments.new(comment_params)
+    @comment.save
+    redirect_to :back
+
+  end
+
+private
+  def comment_params
+    params.permit(:content)
+  end
+end
